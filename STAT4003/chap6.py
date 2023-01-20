364d69e9d393db9f82947df1bafe274f3b272df8
tomleee012 <tom.lee@neuroncloud.ai>
2023-01-20 20:53:23 +0800

    'update'

---
+++ STAT4003/chap6.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+
+# Simulate the correlation coefficient rho from example 
+def SimuRho(mean=5, sd=3):
+    # simulate the data X and Y first
+    return 1
+
+
+if __name__ == "__main__":
+    SimuRho()
