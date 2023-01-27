713a9ae03040dd3539c2689cd852248bf507f032
tomleee012 <tom.lee@neuroncloud.ai>
2023-01-27 15:53:30 +0800

    'update'

--- STAT4003/assg1.py
+++ STAT4003/assg1.py
@@ -1,6 +1,30 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+# convergence in probability
+def ConProb(nRep: int):
+    ds
+
+
 #  Stochastic convergence
 def StoCon():
-    return 1
+    nRep = 2 * 12 # number of simulations
+    nSize = [2 ** i for i in range(5)] # number of sample size
+
+    X = np.random.choice([-1, 1], 1)[0] # the value of X
+    Xn = []
+
+    for iRep in range(nRep):
+        ds
+
+    
+    for n in nSize:
+        In = np.random.binomial(n=1, p=1/n**2) # In ~ Bern(1/j**2)
+        Xn.append( n * In + X * (1 - In)) # representation
+        
+    plt.plot([i for i in range(1, nSize)], Xn, 'b')
+    plt.plot([i for i in range(1, nSize)], [X for i in range(1, nSize)], 'r')
+    plt.show()
 
 if __name__ == "__main__":
     results = StoCon()
